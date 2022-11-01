ac27b789cf82510e8ce3d1b66d90c0cbc59e8cc4
Kenan678 <89321827+Kenan678@users.noreply.github.com>
2022-11-02 00:59:17 +0400

    Autofac

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,19 +1,28 @@
+using Autofac;
+using Autofac.Core;
+using Autofac.Extensions.DependencyInjection;
 using Businness.Abstract;
 using Businness.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using ServiceStack;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Services.AddControllersWithViews();
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterType<Service>().As<IService>());
+
+var app = builder.Build();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
-var app = builder.Build();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,3 +38,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
